45f9869ce8f486ab26be8aa613c379f4b0200cf1
DcSebas95 <sebas13_diaz@hotmail.com>
2026-02-03 00:50:37 -0500

    Creacion Modelo

---
+++ models/Controlfinanciero.cs
@@ -0,0 +1,8 @@
+namespace ZiurBlazorTest.Models;
+
+public class Controlfinanciero
+{
+    public int Codigo { get; set; }
+    public string? Descripcion { get; set; }
+    public bool VActiva { get; set; }
+}
