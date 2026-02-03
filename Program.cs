e96c4b9b1da88fabca64a044cccdcfe1c171262f
DcSebas95 <sebas13_diaz@hotmail.com>
2026-02-03 01:01:12 -0500

    Adicion a program del servicio y crecion de estructura incial

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ZiurBlazorTest;
+//llamado de servicio   
+using ZiurBlazorTest.Services;
+
 
 
 
@@ -10,7 +13,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-
+//Contrucion del servicio
+builder.Services.AddScoped<ControlfinancieroService>();
 
 
 
